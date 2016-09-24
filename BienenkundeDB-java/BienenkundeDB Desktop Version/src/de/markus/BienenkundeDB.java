2d8825ff5ca0e4dbd60ca872959622e4f0da4ace
Sarius997 <sarius997@gmail.com>
2016-09-24 18:20:02 +0200

    changed the version number etc to finally test the update functionality properly

--- BienenkundeDB-java/BienenkundeDB Desktop Version/src/de/markus/BienenkundeDB.java
+++ BienenkundeDB-java/BienenkundeDB Desktop Version/src/de/markus/BienenkundeDB.java
@@ -14,8 +14,8 @@ import de.markus.gui.startWindow;
 import de.markus.updates.Updater;
 
 public class BienenkundeDB {
-	public static final String VERSION = "1.0";
-	public static final String VERSION_NUMBER = "1";
+	public static final String VERSION = "1.0a";
+	public static final String VERSION_NUMBER = "2";
 	// private static PropertiesHandler propHandler = new PropertiesHandler();
 	public static startWindow startWindow;
 	
