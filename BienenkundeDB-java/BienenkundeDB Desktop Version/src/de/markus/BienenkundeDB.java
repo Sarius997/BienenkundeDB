b16e3b59116a3bd6856d5b443ce678415b3f219d
Sarius997 <sarius997@gmail.com>
2017-01-21 22:30:35 +0100

    version information updated for the compatibility code to work

--- BienenkundeDB-java/BienenkundeDB Desktop Version/src/de/markus/BienenkundeDB.java
+++ BienenkundeDB-java/BienenkundeDB Desktop Version/src/de/markus/BienenkundeDB.java
@@ -14,8 +14,8 @@ import de.markus.gui.startWindow;
 import de.markus.updates.Updater;
 
 public class BienenkundeDB {
-	public static final String VERSION = "1.0a";
-	public static final String VERSION_NUMBER = "3";
+	public static final String VERSION = "1.0b";
+	public static final String VERSION_NUMBER = "5";
 
 	public static String LAUNCHER_VERSION_NUMBER = null;
 	// private static PropertiesHandler propHandler = new PropertiesHandler();
